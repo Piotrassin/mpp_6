89c6d6921d56d390ef117613d4a3caa82cdd045c
Piotrassin <piotrassin@gmail.com>
2019-05-23 21:52:34 +0200

    Essentially complete, but performance could be improved. Operating on Strings is slow.

--- src/mainPackage/BruteForce.java
+++ src/mainPackage/BruteForce.java
@@ -10,19 +10,26 @@ class BruteForce {
 
     /**
      * TODO
-     * generator wektorów charakterystycznych
-     * translator wektora, na zbior rozwazanych elementow
-     * obliczanie sumy wag i wartosci zbioru i porownywanie ich z najlepszym do tej pory
+     * DONE     generator wektorów charakterystycznych
+     * DONE     translator wektora, na zbior rozwazanych elementow
+     * DONE     obliczanie sumy wag i wartosci zbioru i porownywanie ich z najlepszym do tej pory
      *
      */
 
-    private List<mainPackage.Element> trainList;
-    private int capacity;
+    private List<mainPackage.Element> elementList;
+    private int backpackCapacity;
+    private int bestValue;
+    private int bestWeight;
+    private String bestCombination;
 
     BruteForce(String trainPath){
-        trainList = loadFile(new File(trainPath));
-        System.out.println(trainList.toString());
+        elementList = loadFile(new File(trainPath));
 
+//        System.out.println(elementList.toString());
+
+        generateVectors(elementList.size());
+
+        System.out.println("Najlepsza kombinacja to " + bestCombination + " o wartosci " + bestValue + " i wadze " + bestWeight);
     }
 
     private List<Element> loadFile(File file) {
@@ -33,7 +40,7 @@ class BruteForce {
         catch (FileNotFoundException e) {e.printStackTrace();}
 
         if (scanner != null) {
-            capacity = Integer.parseInt(scanner.nextLine());
+            backpackCapacity = Integer.parseInt(scanner.nextLine());
             while(scanner.hasNext()) {
                 String input = scanner.nextLine();
                 String[] split = input.split(" ");
@@ -43,5 +50,55 @@ class BruteForce {
         if (scanner != null) { scanner.close(); }
 
         return elementList;
+    }
+
+    private void generateVectors(int elementCount){
+        long combinationCount = (long) Math.pow(2, elementCount);
+        System.out.println("liczba kombinacji " + combinationCount);
+
+        for (int i = 0; i < combinationCount; i++) {
+            String s = "";
+            int j = i;
+
+            if(i % 1000000 == 0){
+                System.out.println("beep");
+            }
+
+            for (int k = 0; k < elementCount; k++) {
+//                s = j % 2 + s;     //ta linia kodu spowalnia program z 800ms do 8000ms dla 10mln obrotow zewn. petli.
+                                    //3500ms bez tej linijki dla wszystkich rozwiązań.
+                j = j / 2;
+            }
+//            System.out.println("s");
+//            translateVectorToCombination(s);
+        }
+    }
+
+    private void translateVectorToCombination(String vector){
+        List<Element> list = new ArrayList<>();
+
+        for (int i = 0; i < vector.length(); i++) {
+            if(vector.charAt(i) == '1') {
+                list.add(elementList.get(i));
+            }
+        }
+        checkCombination(list, vector);
+    }
+
+    private void checkCombination(List<Element> combination, String vector){
+        int totalVal = 0;
+        int totalWeight = 0;
+
+        for (Element e : combination) {
+            totalVal += e.getValue();
+            totalWeight += e.getWeight();
+        }
+        if(totalWeight <= backpackCapacity){
+            if(totalVal > bestValue){
+                bestValue = totalVal;
+                bestWeight = totalWeight;
+                bestCombination = vector;
+            }
+        }
     }
 }
